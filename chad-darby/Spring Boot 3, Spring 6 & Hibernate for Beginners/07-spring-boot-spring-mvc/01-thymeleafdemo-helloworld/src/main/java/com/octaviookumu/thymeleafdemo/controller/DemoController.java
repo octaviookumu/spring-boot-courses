96fa112208ff488b145fdcf1e8c8327625aae683
octaviookumu <octaviookumu@gmail.com>
2024-12-09 22:16:40 +0300

    Hello world form and model

---
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.octaviookumu.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+
+}
