8e64b6c5db44ecf7673713861f2810d50f4e28b3
octaviookumu <octaviookumu@gmail.com>
2025-01-15 12:12:31 +0300

    173. Binding Request Params

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/controller/HelloWorldController.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.octaviookumu.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +18,41 @@ public class HelloWorldController {
     // method to process HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // controller method to read form data
+    // add data to the model
+//    @RequestMapping("/processFormVersionTwo")
+//    public String letsShoutDude(HttpServletRequest request, Model model) {
+//
+//        // read req param from html form
+//        String theName = request.getParameter("studentName");
+//
+//        // convert data to uppercase
+//        String shoutedName = theName.toUpperCase();
+//
+//        // create message
+//        String result = "Yo! " + shoutedName;
+//
+//        // add message to model
+//        model.addAttribute("message", result);
+//
+//        return "helloworld";
+//    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert data to uppercase
+        String shoutedName = theName.toUpperCase();
+
+        // create message
+        String result = "Hey My Friend from v3! " + shoutedName;
+
+        // add message to model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
