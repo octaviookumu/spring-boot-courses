99b51321003f827941bc69b2f5d08c71be5e66a3
octaviookumu <octaviookumu@gmail.com>
2025-01-15 22:03:27 +0300

    189. Data binding - check boxes

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/controller/StudentController.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/controller/StudentController.java
@@ -16,6 +16,12 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -28,6 +34,10 @@ public class StudentController {
         // add list of countries to the model
         theModel.addAttribute("countries", countries);
 
+        // add list of languages to the model
+        theModel.addAttribute("languages", languages);
+
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }
--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/model/Student.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/octaviookumu/thymeleafdemo/model/Student.java
@@ -1,10 +1,30 @@
 package com.octaviookumu.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
+    private String favouriteLanguage;
+    private List<String> favouriteSystems;
+
+    public List<String> getFavouriteSystems() {
+        return favouriteSystems;
+    }
+
+    public void setFavouriteSystems(List<String> favouriteSystems) {
+        this.favouriteSystems = favouriteSystems;
+    }
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
 
     public String getCountry() {
         return country;
