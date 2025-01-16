4fc2c97cc08bdef497367938a5a17d8fecefc350
octavian <octaviookumu@gmail.com>
2025-01-16 16:04:16 +0300

    204. Spring MVC validation - Make Integer fields required

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/octaviookumu/springdemo/mvc/model/Customer.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/octaviookumu/springdemo/mvc/model/Customer.java
@@ -9,9 +9,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses; // using the class 'Integer' instead of the primitive 'int'. Needed to work with @NotNull
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -24,11 +25,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
