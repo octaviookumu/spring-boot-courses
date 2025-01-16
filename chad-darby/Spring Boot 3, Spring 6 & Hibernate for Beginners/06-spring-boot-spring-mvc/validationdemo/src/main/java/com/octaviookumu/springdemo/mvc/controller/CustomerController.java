f594b58982f5613751a9097f9c02449387cdae2d
octavian <octaviookumu@gmail.com>
2025-01-16 15:14:24 +0300

    199. Spring MVC validation - @InitBinder

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/octaviookumu/springdemo/mvc/controller/CustomerController.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/octaviookumu/springdemo/mvc/controller/CustomerController.java
@@ -2,15 +2,25 @@ package com.octaviookumu.springdemo.mvc.controller;
 
 import com.octaviookumu.springdemo.mvc.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder // pre-process all web requests coming into our Controller
+    public void initBinder(WebDataBinder dataBinder) {
+        // StringTrimmerEditor removes whitespace, leading and trailing
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     // Model allows us to share information between controllers and view pages (Thymeleaf)
     @GetMapping("/")
@@ -27,6 +37,7 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult
     ) {
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         }
