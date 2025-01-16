782182f95dbe2425b3ab53da8b1a6983a436d6c5
octavian <octaviookumu@gmail.com>
2025-01-16 15:56:53 +0300

    203. Spring MVC validation - Applying regular expressions

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/octaviookumu/springdemo/mvc/model/Customer.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/octaviookumu/springdemo/mvc/model/Customer.java
@@ -1,9 +1,6 @@
 package com.octaviookumu.springdemo.mvc.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -15,6 +12,17 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getpostalCode() {
+        return postalCode;
+    }
+
+    public void setpostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
