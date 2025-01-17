010be0e4d48cf68ec732eb42c59cc9f9298aa9e1
octaviookumu <octaviookumu@gmail.com>
2025-01-17 17:52:28 +0300

    227. CRUD DB Project - Delete Employee

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -56,6 +53,26 @@ public class EmployeeController {
         // prevent duplicate submissions just in case the user reloads their browser multiple times
         // this is an implementation of a 'Post/Redirect/Get' pattern
         // (see www.luv2code.com.compost-redirect-get)
+        return "redirect:/api/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+        // get employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set the employee as a model attribute to pre-populate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to the form
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+        employeeService.deleteById(theId);
+
         return "redirect:/api/employees/list";
     }
 }
