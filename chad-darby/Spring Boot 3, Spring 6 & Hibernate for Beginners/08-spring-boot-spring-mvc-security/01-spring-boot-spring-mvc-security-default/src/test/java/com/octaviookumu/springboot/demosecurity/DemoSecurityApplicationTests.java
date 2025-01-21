784147d68c96f8ba6eeefae896cf274f57fdddf6
octavian <octaviookumu@gmail.com>
2025-01-21 15:35:38 +0300

    234. Spring MVC Security - Basic Configuration

---
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/octaviookumu/springboot/demosecurity/DemoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octaviookumu.springboot.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
