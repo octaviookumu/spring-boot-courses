6f59e62704dd403d52ced3675f931d5934d7f1ee
octaviookumu <octaviookumu@gmail.com>
2025-02-09 01:13:48 +0300

    245. Spring MVC Security - Logout

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/octaviookumu/springboot/demosecurity/security/DemoSecurityConfig.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/octaviookumu/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.octaviookumu.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -43,7 +44,8 @@ public class DemoSecurityConfig {
                         .loginPage("/showMyLoginPage") // reference custom login form
                         .loginProcessingUrl("/authenticateTheUser")
                         .permitAll()
-                );
+                )
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
