26b4c0c93dcae3f5efbb8dc690cea8ec618aac7f
octaviookumu <octaviookumu@gmail.com>
2025-02-08 22:40:41 +0300

    237. Spring MVC Security - Custom Login Form (SecurityFilterChain)

--- chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/octaviookumu/springboot/demosecurity/security/DemoSecurityConfig.java
+++ chad-darby/Spring Boot 3, Spring 6 & Hibernate for Beginners/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/octaviookumu/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.octaviookumu.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -32,6 +34,18 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
         // spring will use the user details we created in this configuration
+    }
+
+    @Bean
+    private SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage") // reference custom login form
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll()
+                );
+
+        return http.build();
     }
 
 }
